07c5b57f583eb8c4ae850571d6585a17e72c9971
Liobouchan <leo.bouchan@gmail.com>
2013-11-27 23:31:52 -0600

    I start to Comment the code

--- usa_rgb1.c
+++ usa_rgb1.c
@@ -1,15 +1,22 @@
 #include "netpbm.h"
 
 int main(){
-
-  int c1, c2, c3,i,c4,c5,c6,j;
+    
+//Para guardar lo que lea de la imagen y para nuestros for
+  int c1,c2,c3,i,j;
+//Aquí va a guardar nuestra imagen y la va a crear
   netpbm im,il,ie,io;
+  
+//Creamos Las imagenes con sus variables
+// im va a ser para mi imagen a cambiar
   netp_crea(&im,300,400,255,C_AZUL);
-  netp_load("12.ppm",&im);
-
-  //La letra L
+//il es para guardar todo lo referente a la letra L  
   netp_crea(&il,300,400,255,C_AZUL);
+  
+//Cargamos nuestras imagenes 
+  netp_load("12.ppm",&im);
   netp_load("L.ppm",&il);
+
 
     //for(i=0;i < il.anc*il.alt ; ++i){
      //  c1 = il.pix[i].r;
